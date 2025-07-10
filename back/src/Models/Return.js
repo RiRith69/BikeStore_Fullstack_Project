6cb7118b988541aa8cd76c1ddfed915cc4d91368
KjaZin <ysat404@gmail.com>
2025-07-10 22:06:06 +0700

    Aleay

--- back/src/Models/Return.js
+++ back/src/Models/Return.js
@@ -1,6 +1,5 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../DB/database.js";
-import sequelize from "../../database.js";
 
 const Return = sequelize.define("Return", {
   return_date: DataTypes.DATEONLY,
