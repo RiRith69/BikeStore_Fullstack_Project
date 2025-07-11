223b069c95541816ebfa65a701658df88cd9a98e
KjaZin <ysat404@gmail.com>
2025-07-11 16:31:15 +0700

    Kja API

--- back/server2.js
+++ back/server2.js
@@ -2,6 +2,19 @@ import express, { json } from "express";
 import cors from "cors";
 import brandRouter from "./src/Routes/brandRouter.js";
 import productRouter from "./src/Routes/productRouter.js";
+import cartRouter from "./src/Routes/cartRoute.js";
+
+import dotenv from "dotenv";
+import sequelize from "./src/DB/database.js";
+
+import setupAssociation from "./src/Models/association.js"; // ✅ IMPORT IT
+
+dotenv.config();
+
+// ✅ SET UP ASSOCIATIONS BEFORE USING MODELS
+setupAssociation();
+
+await sequelize.sync(); 
 
 const app = express();
 
@@ -9,6 +22,7 @@ app.use(cors());
 app.use(json());
 app.use("/api/brands", brandRouter);
 app.use("/api/products", productRouter);
+app.use("/api/cart", cartRouter)
 
 const PORT = 4000;
 app.listen(PORT, () => {
