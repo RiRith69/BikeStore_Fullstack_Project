0df16b1ea1d9dfed41340a3de576207fffc5d5f1
touchlyheng <touchlyheng50@gmail.com>
2025-07-16 18:27:29 +0700

    Add some file in backend

--- back/server2.js
+++ back/server2.js
@@ -3,28 +3,28 @@ import cors from "cors";
 import brandRouter from "./src/Routes/brandRouter.js";
 import productRouter from "./src/Routes/productRouter.js";
 import cartRouter from "./src/Routes/cartRoute.js";
+import authRouter from "./src/Routes/authRoutes.js";          //ADD THIS
 
 import dotenv from "dotenv";
 import sequelize from "./src/DB/database.js";
-
-import setupAssociation from "./src/Models/association.js"; // ✅ IMPORT IT
+import setupAssociation from "./src/Models/association.js";
 
 dotenv.config();
-
-// ✅ SET UP ASSOCIATIONS BEFORE USING MODELS
 setupAssociation();
-
-await sequelize.sync(); 
+await sequelize.sync();
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+
 app.use("/api/brands", brandRouter);
 app.use("/api/products", productRouter);
-app.use("/api/cart", cartRouter)
+app.use("/api/cart", cartRouter);
+app.use("/api/auth", authRouter);                         //ADD THIS LINE
 
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
