f099cbdb17c8dc00f85018e56e1b316498a85aef
KjaZin <ysat404@gmail.com>
2025-07-17 15:03:49 +0700

    hmmm

--- back/src/Routes/cartRoute.js
+++ back/src/Routes/cartRoute.js
@@ -1,21 +1,11 @@
-// import express from 'express';
-// import * as CartController from "../Controller/cartController.js";
-
-// const router = express.Router();
-// router.get('/', CartController.getCart);
-// router.put('/', CartController.addToCart)
-// router.put('/:id', CartController.updateCartItem);
-// router.delete('/:id', CartController.deleteCartItem);
-
-// export default router;
 
 import express from 'express';
 import * as CartController from "../Controller/cartController.js";
+import { authenticateToken } from "../Middleware/verifyToken.js";
 
 const router = express.Router();
 
 
-// 🛡️ Protected routes
 router.get('/', authenticateToken, CartController.getCart);
 router.put('/', authenticateToken, CartController.addToCart);
 router.put('/:id', authenticateToken, CartController.updateCartItem);
